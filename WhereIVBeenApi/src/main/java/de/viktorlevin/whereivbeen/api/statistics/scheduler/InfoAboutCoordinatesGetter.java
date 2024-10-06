b6543c9ef83d029f10c1d034bc6f29e6f1198ba3
vlevin <vlevin@azbukavkusa.ru>
2024-10-06 20:51:31 +0200

    Added logs for scheduler

--- WhereIVBeenApi/src/main/java/de/viktorlevin/whereivbeen/api/statistics/scheduler/InfoAboutCoordinatesGetter.java
+++ WhereIVBeenApi/src/main/java/de/viktorlevin/whereivbeen/api/statistics/scheduler/InfoAboutCoordinatesGetter.java
@@ -6,12 +6,14 @@ import de.viktorlevin.whereivbeen.api.repository.GeoPointRepository;
 import de.viktorlevin.whereivbeen.api.statistics.coordinatesapi.client.GoogleMapClient;
 import de.viktorlevin.whereivbeen.api.statistics.coordinatesapi.client.dto.MapCountryResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class InfoAboutCoordinatesGetter {
@@ -25,21 +27,28 @@ public class InfoAboutCoordinatesGetter {
     public void getInfoAboutCoordinates() {
         List<GeoPoint> geoPoints = geoPointRepository.findAllByCountryIsNullOrCountryIs("");
         if (geoPoints == null || geoPoints.isEmpty()) {
+            log.info("No geo points found for defining country");
             return;
         }
         GeoPoint first = geoPoints.get(0);
         String ltlg = first.getLatitude() + "," + first.getLongitude();
+        log.info("Trying to define country name for point {}, {}", first.getLatitude(), first.getLongitude());
+        log.info("Going to google API...");
         MapCountryResult result = googleMapClient.getCountryInfoByCoordinates(ltlg, apiKey, "country");
-        if (result.getResults() != null
+        log.info("Received response from Google APi {}", result);
+
+        if (result.getResults() != null //TODO optional
                 && !result.getResults().isEmpty()
                 && result.getResults().get(0).getAddressComponents() != null
                 && !result.getResults().get(0).getAddressComponents().isEmpty()) {
             String country = result.getResults().get(0).getAddressComponents().get(0).getLongName();
             first.setCountry(country);
+            log.info("Country {} defined successfully", country);
         } else {
             first.setCountry("Occupied territory");
         }
 
         geoPointRepository.save(first);
+        log.info("Point was successfully saved to DB with country");
     }
 }
